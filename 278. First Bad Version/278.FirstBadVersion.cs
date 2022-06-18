e2c0bc0930228ac852efc1f961158116da93b73a
Kily <kilynhan@gmail.com>
2022-06-18 11:36:36 -0700

    FirstBadVersion

---
+++ 278. First Bad Version/278.FirstBadVersion.cs
@@ -0,0 +1,17 @@
+public class Solution : VersionControl {
+    public int FirstBadVersion(int n) {
+        
+        int left = 1, right = n;
+        while (left <= right){
+            int mid = left + (right-left)/2;
+            if(IsBadVersion(mid)){
+                
+                right = mid-1;
+            }else{
+                left = mid+1;
+            }
+            
+        }
+        return left;
+    }
+}
